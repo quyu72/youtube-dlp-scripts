0372bedee074f10b29b465cf86cd9ad1b355f439
Quyu <irgikfornik@gmail.com>
2024-11-10 22:42:32 +0300

    Delete Channel directory

--- Channel/download_archive.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-set /p url=Enter URL of the video: 
-yt-dlp --yes-playlist --config-location youtube-dlp.conf "%url%"
-pause
--- Channel/download_archive_multi.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cls
-yt-dlp --yes-playlist --config-location youtube-dlp_multi.conf
-pause
